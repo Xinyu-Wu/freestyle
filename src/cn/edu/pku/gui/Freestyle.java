7d678ba6e41babd676f86edb2ec7ec33c3d2c0b8
Xinyu-Wu <pkuwuxy@pku.edu.cn>
2017-09-25 01:25:12 +0800

    MainFrame Version 0.2

---
+++ src/cn/edu/pku/gui/Freestyle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.edu.pku.gui;
+
+/**
+ *
+ * @author weizy
+ */
+public class Freestyle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Main_win a = new Main_win();
+         a.setVisible(true);
+    }
+    
+}
